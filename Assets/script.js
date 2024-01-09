6f734921398c4bc4d74177f7b4b279051f7ccb8b
Ereyna21 <153467127+Ereyna21@users.noreply.github.com>
2024-01-08 18:15:40 -0600

    save changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,29 @@
-var API = "5538c9736af3a00e4feacc4d6eb29131";
+// var api = "5538c9736af3a00e4feacc4d6eb29131";
+var api = "f30dc0b71f772a037a522282770190be";
+var url ="https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid="+ api;
+var startButton = document.getElementById("weatherApp")
+var inputField = document.getElementById("citySearch")
+
+function startApp(e){
+    e.preventDefault()
+    var cityNameinput = inputField.value
+    getdata(cityNameinput)
+    
+}
+
+
+// beign function to fetch data from api
+
+function getdata(cityName){
+    var lookUpUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&lastupdate&appid=' + api +  "&units=imperial"
+fetch(lookUpUrl)
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(data){
+        console.log(data)
+    })
+
+}
+
+startButton.addEventListener("click" , startApp);
