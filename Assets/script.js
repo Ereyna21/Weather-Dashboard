1a8c7aac1aefdf71cc1cc1bf42c9c320e0326ee9
Ereyna21 <153467127+Ereyna21@users.noreply.github.com>
2024-01-10 23:13:45 -0600

    save changes to html, css and javascript

--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,95 @@
-// var api = "5538c9736af3a00e4feacc4d6eb29131";
-var api = "f30dc0b71f772a037a522282770190be";
-var url ="https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid="+ api;
+var api = "5538c9736af3a00e4feacc4d6eb29131";
+// var api = "f30dc0b71f772a037a522282770190be";
+var url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + api;
 var startButton = document.getElementById("weatherApp")
 var inputField = document.getElementById("citySearch")
+var weatherContainerEl = document.getElementById("weatherContainer")
+var weatherForecastEl = document.getElementById("weatherForecast")
 
-function startApp(e){
+function startApp(e) {
     e.preventDefault()
     var cityNameinput = inputField.value
     getdata(cityNameinput)
-    
+
 }
 
+// Get the current date
+var currentDate = dayjs().format('M/DD/YYYY');
+
+// Display the current date
+document.getElementById('current-date').textContent = currentDate;
 
 // beign function to fetch data from api
 
-function getdata(cityName){
-    var lookUpUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&lastupdate&appid=' + api +  "&units=imperial"
-fetch(lookUpUrl)
-    .then(function(res){
-        return res.json()
-    })
-    .then(function(data){
-        console.log(data)
-    })
+function getdata(cityName) {
+    var lookUpUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&lastupdate&appid=' + api + "&units=imperial"
+    fetch(lookUpUrl)
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+            var citySearched = document.createElement("h3")
+            var tempature = document.createElement("h3")
+            var windSpeed = document.createElement("h3")
+            var humidity = document.createElement("h3")
+            var iconEl = data.weather[0].icon
+            var iconImg = "http://openweathermap.org/img/wn/" + iconEl + ".png"
+            var currentIcon = document.createElement("img")
+            currentIcon.setAttribute("src", iconImg)
+            var lat = data.coord.lat
+            var lon = data.coord.lon
+
+            // Add values to append to page
+            console.log(data)
+            citySearched.textContent = data.name
+            weatherContainerEl.append(citySearched)
+
+            tempature.textContent = data.main.temp + "°F"
+            weatherContainerEl.append(tempature)
+
+            windSpeed.textContent = data.wind.speed + "MPH"
+            weatherContainerEl.append(windSpeed)
+
+            humidity.textContent = data.main.humidity
+            weatherContainerEl.append(humidity)
+
+            weatherContainerEl.append(currentIcon)
+
+            
+
+            // start sub fuction to get weatherforecast
+            var urlForecast = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + api;
+            fetch(urlForecast)
+                .then(function (res) {
+                    return res.json()
+                })
+                .then(function (foreCastData) {
+                    console.log(foreCastData)
+
+
+                    for (var i = 1; i < 6; i++) {
+                        var forecastDate = document.createElement("h3");
+                        forecastDate.textContent = foreCastData.list[i].dt_txt;
+                        weatherForecastEl.append(forecastDate);
+
+                        var forecastTemp = document.createElement("h3");
+                        var tempInKelvin = foreCastData.list[i].main.temp;
+                        var tempInFahrenheit = ((tempInKelvin - 273.15) * 9 / 5) + 32;
+                        forecastTemp.textContent = tempInFahrenheit.toFixed(2) + "°F";
+                        weatherForecastEl.append(forecastTemp);
+
+                        var forecastWind = document.createElement("h3");
+                        forecastWind.textContent = foreCastData.list[i].wind.speed + "MPH";
+                        weatherForecastEl.append(forecastWind);
+
+                        var forecastHumidity = document.createElement("h3");
+                        forecastHumidity.textContent = foreCastData.list[i].main.humidity;
+                        weatherForecastEl.append(forecastHumidity);
+                    }
+                })
+
+        })
 
 }
 
-startButton.addEventListener("click" , startApp);
+startButton.addEventListener("click", startApp);
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Weather-Dashboard
-Challenge-6
+# Weather-Dashboard-Challenge-6
+I was challenged to make a weather app
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+I Made my Document using HTML format CSS styling and Javascript 
+
+I used these websites to refrence codes and help me build my portfolio. https://coding-boot-camp.github.io/full-stack/html/html-cheatsheet https://www.w3schools.com/default.asp https://developer.mozilla.org/en-US/docs/Web/JavaScript https://stackoverflow.com/ https://day.js.org/en/ https://getbootstrap.com/docs/5.1/getting-started/introduction/
+
+
+I have also addded a screenshot ![screenshot](image.png)
+
+Deployed Application link [] 
+
+Repository link https://github.com/Ereyna21/Weather-Dashboard
+
